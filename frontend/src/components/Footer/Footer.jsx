ce5dd8ed3523aff4656f3f1724fb79f459a84217
ola <asz-02@wp.pl>
2026-01-28 00:51:38 +0100

    fix css, imports and router

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
-import "./Footer.module.css";
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div className="footerInner">Contact: aleksandra.szymanska7.stud@pw.edu.pl</div>
+    <footer className={styles.footer}>
+      <div className={styles.footerInner}>Contact: aleksandra.szymanska7.stud@pw.edu.pl</div>
     </footer>
   );
 }
