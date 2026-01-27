3267df3537dc21eb674bc0755b3c76fcbd89e2c0
ola <asz-02@wp.pl>
2026-01-28 00:22:50 +0100

    npm create vite@latest frontend -- --template react rolldown-vite? no npm? yes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import About from "./pages/About/About";
+import FilesList from "./pages/FilesList/FilesList";
+import UploadFiles from "./pages/FilesUpload/FilesUpload";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Navigate to="/about" replace />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/files" element={<FilesList />} />
+        <Route path="/upload" element={<UploadFiles />} />
+      </Route>
+    </Routes>
+  );
+}
